d9c002038913584138d674b2063631f631cc1a56
fearlessfreap24 <dylanaperez@yahoo.com>
2018-05-02 10:58:34 -0500

    forgot to add the files for last commit

---
+++ quit.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dylanperez
+ * Date: 5/2/18
+ * Time: 10:32 AM
+ */
+session_unset();
+session_destroy();
---
+++ start.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dylanperez
+ * Date: 5/2/18
+ * Time: 10:14 AM
+ */
+
+session_start();
+header('Location: factory.html');
+
